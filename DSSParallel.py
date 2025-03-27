ffbe83a2298f677a5df3a8553fbaab4aa9108619
naccurso <naccurso@buffalo.edu>
2025-03-27 13:40:51 -0400

    again...

--- DSSParallel.py
+++ DSSParallel.py
@@ -10,7 +10,7 @@ def LaunchProcess(inst_args):
 
     ArgString = ['DSS.py', f'--n5G={n5G}', f'--nNB={nNB}' f'--RBSet={RBSet}', f'--OutputFile={OutputFilename}']
     print(ArgString)
-    return_code = subprocess.call(['python', ArgString])
+    return_code = subprocess.call(['python', 'DSS.py', f'--n5G={n5G}', f'--nNB={nNB}' f'--RBSet={RBSet}', f'--OutputFile={OutputFilename}'])
     return
 
 
